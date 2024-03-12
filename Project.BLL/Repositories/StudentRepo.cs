3c606ac3fc133cb6ee9ccb88627d7a844c896756
Mariem <mariemmohamed1421@gmail.com>
2024-03-12 14:42:36 +0200

    Adding Instructor & Student Repositories

---
+++ Project.BLL/Repositories/StudentRepo.cs
@@ -0,0 +1,21 @@
+﻿using Project.BLL.Interfaces;
+using Project.DAL.Context;
+using Project.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Repositories
+{
+    public class StudentRepo : GenericRepo<Student>, IstudentRepo
+    {
+        private readonly AppDbContext _context;
+
+        public StudentRepo(AppDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
