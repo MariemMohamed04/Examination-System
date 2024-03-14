fe48126a119fe13e8b45aba6ac313e32c5f156d2
Mariem <mariemmohamed1421@gmail.com>
2024-03-14 13:05:33 +0200

    Adding Update & Delete to UserController

--- Project.PL/Controllers/UserController.cs
+++ Project.PL/Controllers/UserController.cs
@@ -9,8 +9,9 @@ namespace Project.PL.Controllers
     public class UserController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<UserController> _logger;
 
-        public UserController(UserManager<ApplicationUser> userManager)
+        public UserController(UserManager<ApplicationUser> userManager, ILogger<UserController> logger)
         {
             _userManager = userManager;
         }
@@ -26,6 +27,79 @@ namespace Project.PL.Controllers
                 users = await _userManager.Users.Where(user => user.Email.Trim().ToLower().Contains(SearchValue.Trim().ToLower())).ToListAsync();
             }
             return View(users);
+        }
+
+        public async Task<IActionResult> Details(string id, string viewName = "Details")
+        {
+            if (id is null)
+                return NotFound();
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user is null)
+                return NotFound();
+
+            return View(viewName, user);
+        }
+        public async Task<IActionResult> Update(string id)
+        {
+            return await Details(id, "Update");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(string id, ApplicationUser appUser)
+        {
+            if (id != appUser.Id)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = await _userManager.FindByIdAsync(id);
+                    user.UserName = appUser.UserName;
+                    user.NormalizedUserName = appUser.UserName.ToUpper();
+
+                    var result = await _userManager.UpdateAsync(user);
+
+                    if (result.Succeeded)
+                        return RedirectToAction(nameof(Index));
+
+                    foreach (var error in result.Errors)
+                        ModelState.AddModelError(string.Empty, error.Description);
+                }
+                catch (Exception ex)
+                {
+
+                    _logger.LogError(ex.Message);
+                }
+            }
+            return View(appUser);
+        }
+        public async Task<IActionResult> Delete(string id, ApplicationUser appUser)
+        {
+            if (id != appUser.Id)
+                return NotFound();
+
+            try
+            {
+                var user = await _userManager.FindByIdAsync(id);
+
+                var result = await _userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                    return RedirectToAction(nameof(Index));
+
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+                //ViewBag.Errors = result.Errors;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+
+            return View(appUser);
         }
     }
 }
